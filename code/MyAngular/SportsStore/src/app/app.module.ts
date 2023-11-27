cfd68874cf3f66448571785b530c90ca28357550
jahirsheikh <jahirsheikh14@gmail.com>
2023-11-27 13:02:48 +0600

    27-11-2023

--- code/MyAngular/SportsStore/src/app/app.module.ts
+++ code/MyAngular/SportsStore/src/app/app.module.ts
@@ -3,17 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideHttpClient } from '@angular/common/http';
+import { ProductRepository } from './model/product.repository';
+import { StaticDataSource } from './model/static.datasource';
+import { StoreComponent } from './store/store.component';
+import { StoreModule } from './store/store.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule
+  
   ],
   providers: [
-    provideClientHydration()
+    provideHttpClient(),
+    ProductRepository,
+    StaticDataSource,
+    StoreComponent
   ],
   bootstrap: [AppComponent]
 })
