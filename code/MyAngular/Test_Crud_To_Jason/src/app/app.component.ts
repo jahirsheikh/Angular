741688bd2dab2008ec7959fc036c909328f25c77
jahirsheikh <jahirsheikh14@gmail.com>
2023-12-03 19:39:01 +0600

    add 03/12/2023

--- code/MyAngular/Test_Crud_To_Jason/src/app/app.component.ts
+++ code/MyAngular/Test_Crud_To_Jason/src/app/app.component.ts
@@ -13,4 +13,16 @@ export class AppComponent {
   hello='amar sonar bangaldesh'
 
   blogs=['blog 1','blog 2','blog 3']
+
+  number=1;
+
+  imgurl="https://media.licdn.com/dms/image/D5603AQFzjh2QLXeYkg/profile-displayphoto-shrink_200_200/0/1684037664750?e=1706745600&v=beta&t=goH58zRmKqjbu8YG1ha7KCDhi3vST9ort-2_gg_GQ8w";
+
+  onClick(e:any){
+    // console.log('Button Clicked',e)
+
+    this.number--
+
+
+  }
 }
